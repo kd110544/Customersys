cdf8c349a9ab67102f91a7dc6f7c7b2e612b3be7
kd110544 <kd110544@gmail.com>
2020-05-14 14:18:54 +0800

    Create CCustomer

---
+++ app/src/main/java/com/example/customersys/CCustomer.java
@@ -0,0 +1,9 @@
+package com.example.customersys;
+
+public class CCustomer {
+    private String id;
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+}
